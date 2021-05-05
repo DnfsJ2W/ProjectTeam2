c161f28bbc58e8410bda4ce47911c4deea49a813
shivakumar999 <manchikatlashivakumar999@gmail.com>
2021-05-06 00:23:10 +0530

    Resolve Deployment Issues

--- ProductManagementSystem/PMS_WebAPI/Controllers/ProductController.cs
+++ ProductManagementSystem/PMS_WebAPI/Controllers/ProductController.cs
@@ -260,7 +260,8 @@ namespace PMS_WebAPI.Controllers
 
         //Done by Maheshwari
         [HttpDelete]
-        [Route("api/DeleteProduct/{id}")]
+       [Route("api/DeleteProduct/{id}")]
+        // [AcceptVerbs("Get")]
         public void  DeleteProduct(int id)
         { 
             Product product = (from p in db.Products
@@ -284,19 +285,27 @@ namespace PMS_WebAPI.Controllers
         //Done by Maheshwari
         [HttpPost]
         [Route("api/AddOrder")]
-        public int AddOrder([FromBody] Payment payment)
+        public int AddOrder([FromBody] Product product)
         {
+            Payment payments = new Payment();
             Order order = new Order();
             HttpResponseMessage result = null;
             var httpRequest = HttpContext.Current.Request;
-            order.OrderId = Convert.ToInt32(httpRequest["OrderId"]);
+            // order.OrderId = Convert.ToInt32(httpRequest["OrderId"]);
             order.ProductId = Convert.ToInt32(httpRequest["ProductId"]);
             order.ProductQuantity = Convert.ToInt32(httpRequest["ProductQuantity"]);
             order.UserId = Convert.ToInt32(httpRequest["UserId"]);
             order.BookingOn = Convert.ToDateTime(httpRequest["BookingOn"]);
             order.DeliveredOn = Convert.ToDateTime(httpRequest["DeliveredOn"]);
+
+            payments.OrderId = Convert.ToInt32(httpRequest["OrderId"]);
+            payments.UserId = Convert.ToInt32(httpRequest["UserId"]);
+            payments.CardNo = Convert.ToString(httpRequest["CardNo"]);
+            payments.BankName = Convert.ToString(httpRequest["BankName"]);
+            payments.NameOnCard = Convert.ToString(httpRequest["NameOnCard"]);
+            payments.ExpiryDate = Convert.ToDateTime(httpRequest["ExpiryDate"]);
+            db.Payments.Add(payments);
             db.Orders.Add(order);
-            db.Payments.Add(payment);
             db.SaveChanges();
             var max = db.Orders.OrderByDescending(p => p.OrderId).FirstOrDefault().OrderId;
             result = Request.CreateResponse(HttpStatusCode.Created);
